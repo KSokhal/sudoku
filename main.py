440b6779fa65a92c017123c280649b6d1a7db827
KSokhal <kulvir.sokhal@hotmail.com>
2019-12-22 16:39:44 +0000

    Solves Easy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import copy
 
 # Set display dimensions
 DISPLAY_WIDTH = 800
@@ -18,65 +19,70 @@ def main():
     
     # clock = pygame.time.Clock()
 
-    global unsolved_grid
-    unsolved_grid = get_unsolved()
-    solved_grid = unsolved_grid.copy()
+    grid = get_unsolved()
+
     end = False
-    #while True:
-    for i in range(10):
-        solved_grid = unsolved_grid.copy()
-        solved_grid = cycle(solved_grid)
-        print(unsolved_grid == solved_grid)
-        if unsolved_grid == solved_grid:
-            break
-        
-    print_grid(solved_grid)
+
+    while end == False:
+        before = copy.deepcopy(grid)
+        grid = cycle(grid)
+        if before == grid:
+            end = True
+
+    print_grid(grid)
 
 def cycle(grid):
-    solved_grid = grid.copy()
     for row in range(len(grid)):
         for col in range(len(grid[row])):
             if grid[row][col] == 0:
-                common = get_valid_options(row, col)
-                solved_grid = fill_in_easy(common, solved_grid, row, col)
-    return solved_grid
+                common = get_valid_options(grid, row, col)
+                grid = fill_in_easy(grid, row, col, common)
+    return grid
 
-def get_valid_options(row, col):
-    valid, missing = get_check_lists(row, col)
+def get_valid_options(grid, row, col):
+    missing_numbers = get_check_lists(grid, row, col)
 
-    if valid:
-        return
-
-    for i in missing:
+    for i in missing_numbers:
         if len(i) == 1:
-            print(i, row, col)
             return i
 
-    common = set(missing[0])
-    for numbers in missing[1:]:
+    common = set(missing_numbers[0])
+    for numbers in missing_numbers[1:]:
         common.intersection_update(numbers)
 
     common = list(common)
     return common
 
 
-def fill_in_easy(common, grid, row, col):
-    new_grid = grid.copy()
-    if len(common) == 1:
-        new_grid[row][col] = common[0]
-    return new_grid
+def fill_in(grid, row, col, number_options):
+    if len(number_options) == 1:
+        grid[row][col] = number_options[0]
+    else:
+        for option in number_options:
+            grid[row][col] = option
+            
+
+    return grid
 
 
-def get_check_lists(row, col):
-    horizontal_check_list = unsolved_grid[row]
-    vertical_check_list = [x[col] for x in unsolved_grid]
+def get_check_lists(grid, row, col):
+    horizontal_check_list = grid[row]
+    vertical_check_list = [x[col] for x in grid]
+    square_check_list = get_square_list(grid, row, col)
 
+    missing_numbers = []
+    for each_list in [horizontal_check_list, vertical_check_list, square_check_list]:
+        missing_numbers.append(check_list(each_list))
+
+    return missing_numbers
+
+def get_square_list(grid, row, col):
     if row in [0, 1, 2]:
-        square = unsolved_grid[:3]
+        square = grid[:3]
     elif row in [3, 4, 5]:
-        square = unsolved_grid[3:6]
+        square = grid[3:6]
     elif row in [6, 7, 8]:
-        square = unsolved_grid[6:9]
+        square = grid[6:9]
 
     if col in [0, 1, 2]:
         square = [x[:3] for x in square]
@@ -84,38 +90,27 @@ def get_check_lists(row, col):
         square = [x[3:6] for x in square]
     elif col in [6, 7, 8]:
         square = [x[6:9] for x in square]
+    
     square_check_list = []
 
     for i in square:
         for j in i:
             square_check_list.append(j)
 
-    valid = True
-    missing_numbers = []
-
-    for each_list in [horizontal_check_list, vertical_check_list, square_check_list]:
-        temp_valid, temp_missing = check_list(each_list)
-        valid = (valid and temp_valid)
-        missing_numbers.append(temp_missing)
-    if row == 0 and col == 7:
-        print(horizontal_check_list, vertical_check_list, square_check_list)
-        print(missing_numbers)
-    return valid, missing_numbers
+    return square_check_list
 
 
 
 # Check if list is valid and get the numbers that are not present
 def check_list(list):
     missing_numbers = []
-    valid = True
-    for i in range(1, 10):
-        count = list.count(i)
+    for number in range(1, 10):
+        count = list.count(number)
+        if count == 0:
+            missing_numbers.append(number)
         if count > 1:
-            valid = False
-        elif count == 0:
-            valid = False
-            missing_numbers.append(i)
-    return valid, missing_numbers
+            pass
+    return missing_numbers
 
 # Fills in the list with the missing numbers
 def fill_in_list(list, missing_numbers):
