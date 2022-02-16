4f80b7522fe18c28f1ecbe17928c27eec3627209
KSokhal <kulvir.sokhal@hotmail.com>
2022-02-16 13:44:34 +0000

    Updated to use class based grid

--- grid.py
+++ grid.py
@@ -1,9 +1,14 @@
+import time
+
+import pygame
+
+
 class Grid:
-    def __init__(self, fill=False):
-        self.grid = [] 
-        if fill == True:
-            self.get_unsolved_from_txt()
+    def __init__(self):
+        self.grid = []
         
+    def __str__(self):
+        return "\n" + "\n".join([str(x) for x in self.grid]) + "\n"
 
     def get_unsolved_from_txt(self):
         with open('grid.txt') as file:
@@ -12,26 +17,103 @@ class Grid:
                 for number in line.strip("\n"):
                     row.append(int(number))
                 self.grid.append(row)
-    
 
-    def print_txt(self):
-        for i in self.grid:
-            print(i)
-        print("\n")
+    ## Checks if a grid is valid, based on a number entered into a specific cell
+    #
+    # @param row, the row in the grid
+    # @param col, the column of the grid
+    # @param number, the number that has most recently been changed
+    #
+    # @return boolean, if the grid is valid return True
+    def check_if_valid(self, row, col, number):
+        # Checks if all numbers in row occurs only once
+        for i in range(len(self.grid[row])):
+            if self.grid[row][i] == number and col != i:
+                return False
 
+        # Checks if all numbers in column occurs only once
+        for i in range(len(self.grid)):
+            if self.grid[i][col] == number and row != i:
+                return False
 
+        # Defines the 3x3 grid that needs to be checked
+        square = [(row // 3) * 3, (col//3) * 3]
+        
+        # Checks if all numbers in the 3x3 square occurs only once
+        for i in range(square[0] , square[0] + 3):
+            for j in range(square[1], square[1] + 3):
+                if number == self.grid[i][j] and i != row and j != col:
+                    return False
+        return True
+
+    ## Gets the next empty cell in the grid
+    #
+    # @return coordinates of empty cell if found, None if no empty cells
     def get_next_empty_cell(self):
         for row in range(len(self.grid)):
             for col in range(len(self.grid[0])):
                 if self.grid[row][col] == 0:
                     return (row, col)
         return None
-<<<<<<< HEAD
+
+    def solve(self, drawer, display):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        # If an empty cell can be found contiue filling in grid, else return True
+        if self.get_next_empty_cell() is not None:
+            # Get the next empty cell
+            empty_row, empty_col = self.get_next_empty_cell()
+            for i in range(1, 10):
+                # If the value of i is a valid option for the empty cell, enter it in
+                if self.check_if_valid( empty_row, empty_col, i):
+                    self.grid[empty_row][empty_col] = i
+                    # If the grid can be solved from this point continue solving, else reset the cell and try next number
+                    # drawer(display, self.grid)
+                    drawer(display, empty_row, empty_col, i, text_colour = (0, 255, 0))
+                    time.sleep(0.1)
+                    if self.solve(drawer, display):
+                        return True
+                    drawer(display, empty_row, empty_col, i, text_colour = (255, 0, 0))
+                    time.sleep(0.1)
+                    
+                    self.grid[empty_row][empty_col] = 0
+                    # drawer(display, self.grid)
+                    drawer(display, empty_row, empty_col, 0, text_colour = (255, 0, 0))
+
+            return False       
+        else:
+            return True
 
 
-if __name__ == "__main__":
-    sudoku_grid = Grid(fill=True)
-    sudoku_grid.get_unsolved_from_txt()
-    sudoku_grid.print_txt()
-=======
->>>>>>> 15099d0240144f12b8f71682190ad7684591ae7f
+    ## Checks if a grid is valid as a whole
+    #
+    # @param grid, nestesd lists that represent a 9x9 grid of intergers
+    #
+    # @return boolean, if the grid is valid return True
+    def check(self):
+        for row in self.grid:
+            for i in range(1, 10):
+                if row.count(i) != 1:
+                    return False
+
+        for col in range(9):
+            lst = [row[col] for row in self.grid]
+            for i in range(1, 10):
+                if lst.count(i) != 1:
+                    return False
+        
+        for i in range(3):
+            for j in range(3):
+                lst = [row[j* 3:(j*3) + 3] for row in self.grid[i * 3:(i*3) + 3]] 
+                flat_list = []
+                for k in lst:
+                    for number in k:
+                        flat_list.append(number)
+                
+                for check_number in range(1, 10):
+                    if flat_list.count(check_number) != 1:
+                        return False
+        return True
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import copy
 import time
 from math import floor
+from grid import Grid
 
 import pygame
 
@@ -23,8 +24,9 @@ CELL_SIZE = 50
 
 def main():
     # Generates grids from text file
-    grid = get_unsolved()
-    original_grid = copy.deepcopy(grid)
+    grid = Grid()
+    grid.get_unsolved_from_txt()
+    original_grid = copy.deepcopy(grid.grid)
 
     # Initialises varibles for pygame
     pygame.init()
@@ -35,7 +37,7 @@ def main():
     pygame.display.set_caption("Sudoku")
     game_display.fill(WHITE)
     clock = pygame.time.Clock()
-    draw_grid(game_display, grid)
+    draw_grid(game_display, grid.grid)
 
     end = False
     allow_edit = False
@@ -47,13 +49,14 @@ def main():
                 end = True
         
         # Draws 'sovle' button and wait for it to be pressed
-        if button(game_display, (GRID_START_POS[0] + (10 * CELL_SIZE), GRID_START_POS[1] + CELL_SIZE), ((2 * CELL_SIZE), CELL_SIZE), LIGHT_GREY, DARK_GREY, "Solve"):
+        btn = button(game_display, (GRID_START_POS[0] + (10 * CELL_SIZE), GRID_START_POS[1] + CELL_SIZE), ((2 * CELL_SIZE), CELL_SIZE), LIGHT_GREY, DARK_GREY, "Solve")
+        if btn:
             # If button is pressed, display original grid and solve
-            draw_grid(game_display, original_grid)
-            fill_in_grid(game_display, original_grid)
+            draw_grid(game_display, grid.grid)
+            grid.solve(draw_cell, game_display)
 
         # Checks if the grid has been manually completed
-        if check(grid):
+        if grid.check():
             text = font.render("Level Complete!", True, GREEN)
             exit_str = "Press any key to quit..."
             small_text = small_font.render(exit_str , True, BLACK)
@@ -110,7 +113,7 @@ def main():
                 pygame.draw.rect(game_display, WHITE, (GRID_START_POS[0] + 10 + (x * CELL_SIZE), GRID_START_POS[1] + 10 + (y * CELL_SIZE), CELL_SIZE - 20, CELL_SIZE - 20), 0)
                 
                 if number != "0":
-                    if check_if_valid(grid, y, x, int(number)):
+                    if grid.check_if_valid(y, x, int(number)):
                         text_colour = BLACK
                     else:
                         text_colour = RED
@@ -128,27 +131,34 @@ def main():
     quit()
 
 
-## Draws a grid
-#
-# @param display, the pygame display
-# @param grid, nestesd lists that represent a 9x9 grid of intergers
+
+def draw_cell(display, col, row , value, text_colour = BLACK, update=True):
+    pygame.draw.rect(display, BLACK, (GRID_START_POS[0] + (row * CELL_SIZE), GRID_START_POS[1] + (col * CELL_SIZE), CELL_SIZE, CELL_SIZE), 1)
+    value = str(value)
+    if value == "0":
+        pygame.draw.rect(display, WHITE, (GRID_START_POS[0] + 10 + (row * CELL_SIZE), GRID_START_POS[1] + 10 + (col * CELL_SIZE), CELL_SIZE - 20, CELL_SIZE - 20), 0)
+    else:
+        text = font.render(value, True, text_colour)
+        text_size = font.size(value)
+        text_start_pos = (round((CELL_SIZE - text_size[0]) / 2), round((CELL_SIZE - text_size[1]) / 2))
+        display.blit(text, (GRID_START_POS[0] + text_start_pos[0] + (row * CELL_SIZE), GRID_START_POS[1] + text_start_pos[1] + (col * CELL_SIZE)))
+    if update:
+        pygame.display.update()
+
+
+# ## Draws a grid
+# #
+# # @param display, the pygame display
+# # @param grid, nestesd lists that represent a 9x9 grid of intergers
 
 def draw_grid(display, grid):
     for row in range(len(grid)):
         for col in range(len(grid[row])):
-            pygame.draw.rect(display, BLACK, (GRID_START_POS[0] + (row * CELL_SIZE), GRID_START_POS[1] + (col * CELL_SIZE), CELL_SIZE, CELL_SIZE), 1)
             cell_value = str(grid[col][row])
-            if cell_value == "0":
-                pygame.draw.rect(display, WHITE, (GRID_START_POS[0] + 10 + (row * CELL_SIZE), GRID_START_POS[1] + 10 + (col * CELL_SIZE), CELL_SIZE - 20, CELL_SIZE - 20), 0)
-            else:
-                text = font.render(cell_value, True, BLACK)
-                text_size = font.size(cell_value)
-                text_start_pos = (round((CELL_SIZE - text_size[0]) / 2), round((CELL_SIZE - text_size[1]) / 2))
-                display.blit(text, (GRID_START_POS[0] + text_start_pos[0] + (row * CELL_SIZE), GRID_START_POS[1] + text_start_pos[1] + (col * CELL_SIZE)))
+            draw_cell(display, col, row , cell_value, update=False)
     for i in range(3):
         for j in range(3):
             pygame.draw.rect(display, BLACK, (GRID_START_POS[0] + (i * CELL_SIZE * 3), GRID_START_POS[1] + (j * CELL_SIZE * 3), CELL_SIZE * 3, CELL_SIZE * 3), 5)
-
     pygame.display.update()
 
 
@@ -186,137 +196,6 @@ def button(display, pos, size, active_colour, inactive_colour, display_text):
         return True
     else:
         return False
-
-
-## Checks if a grid is valid as a whole
-#
-# @param grid, nestesd lists that represent a 9x9 grid of intergers
-#
-# @return boolean, if the grid is valid return True
-
-def check(grid):
-    for row in grid:
-        for i in range(1, 10):
-            if row.count(i) != 1:
-                return False
-
-    for col in range(9):
-        lst = [row[col] for row in grid]
-        for i in range(1, 10):
-            if lst.count(i) != 1:
-                return False
-    
-    for i in range(3):
-        for j in range(3):
-            lst = [row[j* 3:(j*3) + 3] for row in grid[i * 3:(i*3) + 3]] 
-            flat_list = []
-            for k in lst:
-                for number in k:
-                    flat_list.append(number)
-            
-            for check_number in range(1, 10):
-                if flat_list.count(check_number) != 1:
-                    return False
-    return True
-
-
-## Checks if a grid is valid, based on a number entered into a specific cell
-#
-# @param grid, nestesd lists that represent a 9x9 grid of intergers
-# @param row, the row in the grid
-# @param col, the column of the grid
-# @param number, the number that has most recently been changed
-#
-# @return boolean, if the grid is valid return True
-
-def check_if_valid(grid, row, col, number):
-    # Checks if all numbers in row occurs only once
-    for i in range(len(grid[row])):
-        if grid[row][i] == number and col != i:
-            return False
-
-    # Checks if all numbers in column occurs only once
-    for i in range(len(grid)):
-        if grid[i][col] == number and row != i:
-            return False
-
-    # Defines the 3x3 grid that needs to be checked
-    square = [(row // 3) * 3, (col//3) * 3]
-    
-    # Checks if all numbers in the 3x3 square occurs only once
-    for i in range(square[0] , square[0] + 3):
-        for j in range(square[1], square[1] + 3):
-            if number == grid[i][j] and i != row and j != col:
-                return False
-    return True
-
-
-## Gets the next empty cell in the grid
-#
-# @param grid, nestesd lists that represent a 9x9 grid of intergers
-#
-# @return coordinates of empty cell if found, None if no empty cells
-
-def get_next_empty_cell(grid):
-    for row in range(len(grid)):
-        for col in range(len(grid[0])):
-            if grid[row][col] == 0:
-                return (row, col)
-    return None
-
-# Attempts to solve the grid
-#
-# @param grid, nestesd lists that represent a 9x9 grid of intergers
-#
-# @return boolean, if the grid was solved True
-
-def fill_in_grid(display, grid):
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-
-
-    # If an empty cell can be found contiue filling in grid, else return True
-    if get_next_empty_cell(grid) is not None:
-        # Get the next empty cell
-        empty_row, empty_col = get_next_empty_cell(grid)
-        for i in range(1, 10):
-            # If the value of i is a valid option for the empty cell, enter it in
-            if check_if_valid(grid, empty_row, empty_col, i):
-                grid[empty_row][empty_col] = i
-                # If the grid can be solved from this point continue solving, else reset the cell and try next number
-                draw_grid(display, grid)
-                time.sleep(0.1)
-                if fill_in_grid(display, grid):
-                    return True
-                grid[empty_row][empty_col] = 0
-                draw_grid(display, grid)
-        return False       
-    else:
-        return True
-
-
-# Creats grid from text on notepad
-#
-# @return grid, nestesd lists that represent a 9x9 grid of intergers
-
-def get_unsolved():
-    grid = []
-    with open("grid.txt") as file:
-        for line in file.readlines():
-            row = []
-            for number in line.strip("\n"):
-                row.append(int(number))
-            grid.append(row)
-    return grid
-
-
-# Prints grid in easier to see manor
-def print_text_grid(grid):
-    [print(i) for i in grid]
-    print("\n")
 
 
 if __name__ == "__main__":
